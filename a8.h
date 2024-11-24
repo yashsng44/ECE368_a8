8829ecae3d06fca3170b1d2f06450245053dd6db
yashsng44 <136346680+yashsng44@users.noreply.github.com>
2024-11-24 14:42:13 -0500

    Update a8.h

--- a8.h
+++ a8.h
@@ -1,5 +1,5 @@
-#ifndef a6_h
-#define a6_h 
+#ifndef a8_h
+#define a8_h 
 #include <stdio.h>  
 #include <stdlib.h> 
 #include <string.h> 
@@ -15,19 +15,23 @@ typedef struct adj_Vertex {
 
 typedef struct adj_Edges {
     int name;
-    int weight;
-    int depth_to;
+    int * weights;
 } Edges;
 
-// typedef struct Queue {
-//     Node * front, *rear;
-// } 
+typedef struct {
+    int vertex;
+    int depth;  // Track depth explicitly for weight cycles
+    int cost;
+} QueueNode;
 
-// typedef struct Node {
-//     int name;
-//     struct Node * next;
-// }
+typedef struct {
+    QueueNode* nodes;
+    int* positions;  // Track positions in heap for faster updates
+    int size;
+    int capacity;
+} PriorityQueue;
 
 struct adj_Vertex * build_adjacency_list(char * filename, int * vertices, int * period);
-void djk_mul_weights(struct adj_Vertex * adj_list, int source, int target, int vertices);
+void djk_mul_weights(struct adj_Vertex * adj_list, int source, int target, int vertices, int period);
+void free_adj_list(struct adj_Vertex * adj_list, int vertices, int period);
 #endif
