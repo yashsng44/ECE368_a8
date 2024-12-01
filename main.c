cf3f9a64adb2a15fc38eaf63eab07f302939f799
yashsng44 <136346680+yashsng44@users.noreply.github.com>
2024-12-01 17:06:56 -0500

    Update main.c

--- main.c
+++ main.c
@@ -3,24 +3,26 @@
 #include <string.h> 
 #include <stdbool.h>
 #include <math.h>
-#include "a8.h"
+#include "a8.h" // Ensure this header file is correct and included in your project
 
-int main(int argc, char ** argv) {
 
-if (argc != 2)
-    {
-      return EXIT_FAILURE;
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        return EXIT_FAILURE;
+    }
+
+    int V, N, source, target;
+
+    struct adj_Vertex *adj_list = build_adjacency_list(argv[1], &V, &N); // Build the graph
+
+    while(scanf("\n%d %d", &source, &target) == 2) {
+        if (target > (V - 1) || target < 0 || source > (V - 1) || source < 0) {
+            break;
+        }
+        find_shortest_path(adj_list, source, target, V, N);
+    }
+
+
+    free_adjacency_list(adj_list, V);
+    return EXIT_SUCCESS;
 }
-
-int vertices, period, source, target = 0;
-struct adj_Vertex * adj_list = build_adjacency_list(argv[1], &vertices, &period); // build the graph
-
-while(scanf("\n%d %d", &source, &target) == 2) {
-  djk_mul_weights(adj_list, source, target, vertices, period);
-}
-
-free_adj_list(adj_list, vertices, period);
-return EXIT_SUCCESS;
-
-}
-    
